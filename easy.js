fd556b6925839b6419b394dcf0f8f1c8d1b22974
Dawid Marszalik <proconqpa@gmail.com>
2020-08-04 18:30:41 +0200

    Added posibility to insert shape in the field and some small staff

--- easy.js
+++ easy.js
@@ -1,6 +1,8 @@
 const active = document.querySelector('.active');
 const jet = document.createElement('i');
 const inactive = document.querySelector('.inactive')
+
+//tworzy link który przenosi w tryb gry trudny
 inactive.setAttribute('onclick', 'location.href = "hard.html"');
 
 
@@ -11,3 +13,37 @@ jet.setAttribute('class', 'fas fa-fighter-jet');
 const parent = active.parentNode;
 //dodaje nowy węzeł (jet), przed elementem istniejącym (active)
 parent.insertBefore(jet, active);
+
+//dodaje flagę żeby umożliwić zmiany kształtu gracza (kółko lub krzyżyk)
+let flag = false;
+
+const a1 = document.getElementById('a1');
+const a2 = document.getElementById('a2');
+const a3 = document.getElementById('a3');
+const b1 = document.getElementById('b1');
+const b2 = document.getElementById('b2');
+const b3 = document.getElementById('b3');
+const c1 = document.getElementById('c1');
+const c2 = document.getElementById('c2');
+const c3 = document.getElementById('c3');
+
+[a1, a2, a3, b1, b2, b3, c1, c2, c3].forEach((item) => {
+    item.addEventListener('click', () => {
+        /* funkcja która dodaje kółko lub krzyżyk w klikniętym polu */
+        if (item.childElementCount == 0) {
+            /* instrukcja warunkowa zapobiegająca dodawaniu więcej niż jednej figuty w jednym polu */
+            const shape = document.createElement('i');
+            if (flag) {
+                /* instrukcja warunkowa która decyduje jaką figurę ma aktualnie gracz */
+                shape.setAttribute('class', 'far fa-circle');
+            } else {
+                shape.setAttribute('class', 'fas fa-times');
+            }
+            // console.log(item.childElementCount)
+            item.appendChild(shape);
+        }
+
+
+
+    })
+})
